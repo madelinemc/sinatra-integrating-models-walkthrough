530027783160e2defcbd1f38e1b0c6bcef062423
madelinemc <madeline.coons@gmail.com>
2020-11-11 10:45:04 -0600

    commit tree 083590d5005707fb09ddc2b14a3c98610839f069

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative 'models/text_analyzer.rb' # connecting the TextAnalyzer model to the controller. gives ability to reference TextAnalyzer calss and invoke its new method.
 
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
+  #renders the index page
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text]) #taking data from params and feeding it into a new instance of TextAnalyzer class.
+    # the instance of TextAnalyzer is saved to the instance variable @analyzed_text which means it and its methods from results.erb
     erb :results
   end
+  #receives the form data through params and renders the results page.
+
 end
